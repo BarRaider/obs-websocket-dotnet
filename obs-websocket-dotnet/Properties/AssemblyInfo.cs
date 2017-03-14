5d52e50ac4ca8693ee82e5fb9b7bab4fbfcc3ddc
Palakis <contact@slepin.fr>
2017-03-14 16:35:16 +0100

    Name changes continued

--- obs-websocket-dotnet/Properties/AssemblyInfo.cs
+++ obs-websocket-dotnet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("obs-websocket-sharp")]
+[assembly: AssemblyTitle("obs-websocket-dotnet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("obs-websocket-sharp")]
+[assembly: AssemblyProduct("obs-websocket-dotnet")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
