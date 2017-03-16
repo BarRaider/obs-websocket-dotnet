3d8905c6e69822fa8a5e623dfdbb5e30e514e39c
Palakis <contact@slepin.fr>
2017-03-16 10:45:41 +0100

    Version bump

--- obs-websocket-dotnet/Properties/AssemblyInfo.cs
+++ obs-websocket-dotnet/Properties/AssemblyInfo.cs
@@ -32,5 +32,8 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: InternalsVisibleTo("obs-websocket-dotnet.Tests")]
+[assembly: InternalsVisibleTo("obs-websocket-dotnet.Explorables")]
+
