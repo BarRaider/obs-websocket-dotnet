67771a3998b473f6684e5e6fdc2fd93d4a99ae38
Tino Donderwinkel <tinodo@microsoft.com>
2021-02-15 16:08:52 +0100

    Added TriggerHotkeyByName and TriggerHotkeyBySequence

---
+++ obs-websocket-dotnet/KeyModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OBSWebsocketDotNet
+{
+    [Flags]
+    /// <summary>
+    /// Key Modifiers
+    /// </summary>
+    public enum KeyModifier
+    {
+        None    = 0b_0000_0000,  // 0
+        Shift   = 0b_0000_0001,  // 1
+        Alt     = 0b_0000_0010,  // 2
+        Control = 0b_0000_0100,  // 4,
+        Command = 0b_0000_1000   // 8
+    }
+}
--- obs-websocket-dotnet/OBSWebsocket_Requests.cs
+++ obs-websocket-dotnet/OBSWebsocket_Requests.cs
@@ -101,6 +101,41 @@ namespace OBSWebsocketDotNet
         }
 
         /// <summary>
+        /// Executes hotkey routine, identified by hotkey unique name
+        /// </summary>
+        /// <param name="hotkeyName">Unique name of the hotkey, as defined when registering the hotkey (e.g. "ReplayBuffer.Save")</param>
+        public void TriggerHotkeyByName(string hotkeyName)
+        {
+            var requestFields = new JObject
+            {
+                { "hotkeyName", hotkeyName }
+            };
+
+            SendRequest("TriggerHotkeyByName", requestFields);
+        }
+
+        /// <summary>
+        /// EExecutes hotkey routine, identified by bound combination of keys. A single key combination might trigger multiple hotkey routines depending on user settings
+        /// </summary>
+        /// <param name="keyId">Main key identifier (e.g. OBS_KEY_A for key "A"). Available identifiers are here: https://github.com/obsproject/obs-studio/blob/master/libobs/obs-hotkeys.h</param>
+        /// <param name="keyModifier">Optional key modifiers object. You can combine multiple key operators. e.g. KeyModifier.Shift | KeyModifier.Control</param>
+        public void TriggerHotkeyBySequence(string keyId, KeyModifier keyModifier = KeyModifier.None)
+        {
+            var requestFields = new JObject
+            {
+                { "keyId", keyId },
+                { "keyModifiers", new JObject{
+                    { "shift", (keyModifier & KeyModifier.Shift) == KeyModifier.Shift },
+                    { "alt", (keyModifier & KeyModifier.Alt) == KeyModifier.Alt },
+                    { "control", (keyModifier & KeyModifier.Control) == KeyModifier.Control },
+                    { "command", (keyModifier & KeyModifier.Command) == KeyModifier.Command } } 
+                }
+            };
+
+            SendRequest("TriggerHotkeyBySequence", requestFields);
+        }
+
+        /// <summary>
         /// Get the current scene info along with its items
         /// </summary>
         /// <returns>An <see cref="OBSScene"/> object describing the current scene</returns>
