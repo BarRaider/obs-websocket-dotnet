74af2d897e781431e023e71db56a127e6ef404af
BarRaider <46548278+BarRaider@users.noreply.github.com>
2021-03-17 15:39:29 -0700

    Minor 'Spring cleaning' and filesystem refactoring

--- obs-websocket-dotnet/Events.cs
+++ obs-websocket-dotnet/Events.cs
@@ -1,35 +1,9 @@
-﻿/*
-    The MIT License (MIT)
-
-    Copyright (c) 2017 Stéphane Lepin
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
-
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using OBSWebsocketDotNet.Types;
-using System;
 using System.Collections.Generic;
 
 namespace OBSWebsocketDotNet
 {
-
     /// <summary>
     /// Called by <see cref="OBSWebsocket.SceneChanged"/>
     /// </summary>
@@ -351,25 +325,4 @@ namespace OBSWebsocketDotNet
     /// <param name="sourceName">Name of source</param>
     /// <param name="sourceKind">Kind of source</param>
     public delegate void MediaPlayingCallback(OBSWebsocket sender, string sourceName, string sourceKind);
-
-    /// <summary>
-    /// Thrown if authentication fails
-    /// </summary>
-    public class AuthFailureException : Exception
-    {
-    }
-
-    /// <summary>
-    /// Thrown when the server responds with an error
-    /// </summary>
-    public class ErrorResponseException : Exception
-    {
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="message"></param>
-        public ErrorResponseException(string message) : base(message)
-        {
-        }
-    }
 }
