0d437a4e5eefa2e551fb4595c977d6ac12a9ade6
Salman Muin Kayser Chishti <13schishti@gmail.com>
2021-05-13 14:41:05 +0100

    name of button change

--- TestClient/MainWindow.Designer.cs
+++ TestClient/MainWindow.Designer.cs
@@ -109,7 +109,7 @@ namespace TestClient
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.gbControls.SuspendLayout();
             this.groupBox7.SuspendLayout();
@@ -257,7 +257,7 @@ namespace TestClient
             this.gbControls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gbControls.Controls.Add(this.button2);
+            this.gbControls.Controls.Add(this.browseButton);
             this.gbControls.Controls.Add(this.folderpath);
             this.gbControls.Controls.Add(this.button1);
             this.gbControls.Controls.Add(this.btnAdvanced);
@@ -939,16 +939,16 @@ namespace TestClient
             this.button3.Text = "Set Path";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // browseButton
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(260, 616);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 23);
-            this.button2.TabIndex = 14;
-            this.button2.Text = "Set Path";
-            this.button2.UseVisualStyleBackColor = true;
+            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseButton.Location = new System.Drawing.Point(260, 616);
+            this.browseButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(88, 23);
+            this.browseButton.TabIndex = 14;
+            this.browseButton.Text = "Set Path";
+            this.browseButton.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
@@ -1051,7 +1051,7 @@ namespace TestClient
         private System.Windows.Forms.Button btnAdvanced;
         private System.Windows.Forms.TextBox folderpath;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button browseButton;
         private System.Windows.Forms.Button button3;
     }
 }
