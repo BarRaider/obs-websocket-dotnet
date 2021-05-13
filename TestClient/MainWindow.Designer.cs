1b7a6f34b75f3c1efc11a2233c762aec0e4e49a1
Salman Muin Kayser Chishti <13schishti@gmail.com>
2021-05-13 14:29:28 +0100

    moved button down

--- TestClient/MainWindow.Designer.cs
+++ TestClient/MainWindow.Designer.cs
@@ -278,7 +278,7 @@ namespace TestClient
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(148, -9);
+            this.button1.Location = new System.Drawing.Point(241, 616);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 27);
