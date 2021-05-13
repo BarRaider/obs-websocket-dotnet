6438c38b44a47e4abcb0cce469ac11a2c6240fef
Salman Muin Kayser Chishti <13schishti@gmail.com>
2021-05-13 14:29:46 +0100

    button resize

--- TestClient/MainWindow.Designer.cs
+++ TestClient/MainWindow.Designer.cs
@@ -281,7 +281,7 @@ namespace TestClient
             this.button1.Location = new System.Drawing.Point(241, 616);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 27);
+            this.button1.Size = new System.Drawing.Size(88, 23);
             this.button1.TabIndex = 11;
             this.button1.Text = "Connect";
             this.button1.UseVisualStyleBackColor = true;
