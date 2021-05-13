5a06478508f553d0683f335b965a1f5a00e10223
Salman Muin Kayser Chishti <13schishti@gmail.com>
2021-05-13 14:35:47 +0100

    set path button name instead

--- TestClient/MainWindow.Designer.cs
+++ TestClient/MainWindow.Designer.cs
@@ -293,7 +293,7 @@ namespace TestClient
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 23);
             this.button1.TabIndex = 11;
-            this.button1.Text = "Connect";
+            this.button1.Text = "Set Path";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
