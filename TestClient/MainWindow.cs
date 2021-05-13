c1c303769313925bd9ca0c8a000a0a666c0d8e0b
Salman Muin Kayser Chishti <13schishti@gmail.com>
2021-05-13 14:20:40 +0100

    remove old method

--- TestClient/MainWindow.cs
+++ TestClient/MainWindow.cs
@@ -397,12 +397,7 @@ namespace TestClient
             advanced.Show();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+          private void button1_Click(object sender, EventArgs e)
         {
 
         }
