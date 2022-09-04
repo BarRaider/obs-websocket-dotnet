f1b3edc6f0591d131d5befdcf20f1f8207f5a72f
BarRaider <46548278+BarRaider@users.noreply.github.com>
2022-09-04 09:11:55 +0300

    Fix GetSceneList for v5

--- obs-websocket-dotnet/Types/GetSceneListInfo.cs
+++ obs-websocket-dotnet/Types/GetSceneListInfo.cs
@@ -4,15 +4,22 @@ using System.Collections.Generic;
 namespace OBSWebsocketDotNet.Types
 {
     /// <summary>
-    /// Get Scene Info response
+    /// Response from <see cref="OBSWebsocket.GetSceneList"/>
     /// </summary>
     public class GetSceneListInfo
     {
         /// <summary>
-        /// Name of the currently active scene
+        /// Name of the currently active program scene
         /// </summary>
-        [JsonProperty(PropertyName = "current-scene")]
-        public string CurrentScene { set; get; }
+        [JsonProperty(PropertyName = "currentProgramSceneName")]
+        public string CurrentProgramSceneName { set; get; }
+
+        /// <summary>
+        /// Name of the currently active preview/studio scene
+        /// Note: Will return null if not in studio mode
+        /// </summary>
+        [JsonProperty(PropertyName = "currentPreviewSceneName")]
+        public string CurrentPreviewSceneName { set; get; }
 
         /// <summary>
         /// Ordered list of the current profile's scenes
