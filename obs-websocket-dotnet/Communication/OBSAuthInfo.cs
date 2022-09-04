b66dcea8d78f5c5ade00fdac830ce4c1b8b7f012
BarRaider <46548278+BarRaider@users.noreply.github.com>
2022-09-05 00:23:05 +0300

    Move to Communication namespace

---
+++ obs-websocket-dotnet/Communication/OBSAuthInfo.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OBSWebsocketDotNet.Communication
+{
+    /// <summary>
+    /// Data required by authentication
+    /// </summary>
+    public class OBSAuthInfo
+    {
+        /// <summary>
+        /// Authentication challenge
+        /// </summary>
+        [JsonProperty(PropertyName = "challenge")]
+        public readonly string Challenge;
+
+        /// <summary>
+        /// Password salt
+        /// </summary>
+        [JsonProperty(PropertyName = "salt")]
+        public readonly string PasswordSalt;
+
+        /// <summary>
+        /// Builds the object from JSON response body
+        /// </summary>
+        /// <param name="data">JSON response body as a <see cref="JObject"/></param>
+        public OBSAuthInfo(JObject data)
+        {
+            JsonConvert.PopulateObject(data.ToString(), this);
+        }
+
+        /// <summary>
+        /// Default Constructor for deserialization
+        /// </summary>
+        public OBSAuthInfo() { }
+    }
+}
---
+++ obs-websocket-dotnet/Communication/ServerMessage.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace OBSWebsocketDotNet.Communication
+{
+    /// <summary>
+    /// Message received from the server
+    /// </summary>
+    internal class ServerMessage
+    {
+        /// <summary>
+        /// Server Message's operation code
+        /// </summary>
+        [JsonProperty(PropertyName = "op")]
+        public MessageTypes OperationCode { set; get; }
+
+        /// <summary>
+        /// Server Data
+        /// </summary>
+        [JsonProperty(PropertyName = "d")]
+        public JObject Data { get; set; }
+    }
+}
