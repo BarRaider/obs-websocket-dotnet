c48737d254d6173130f451409676693571f20e1f
Steven Ryland <stevenry@outlook.com>
2024-05-14 21:31:05 -0700

    Creating enum for MediaState and allowing OBSVideoSettings to be configured

--- obs-websocket-dotnet/Types/MediaInputStatus.cs
+++ obs-websocket-dotnet/Types/MediaInputStatus.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace OBSWebsocketDotNet.Types
@@ -12,19 +13,34 @@ namespace OBSWebsocketDotNet.Types
         /// State of the media input
         /// </summary>
         [JsonProperty(PropertyName = "mediaState")]
-        public string State { get; set; }
+        internal string StateString { get; set; }
+
+        /// <summary>
+        /// State of the media input
+        /// </summary>
+        public MediaState? State
+        {
+            get
+            {
+                if (!Enum.TryParse(StateString, out MediaState state))
+                {
+                    return null;
+                }
+                return state;
+            }
+        }
 
         /// <summary>
         /// Total duration of the playing media in milliseconds. `null` if not playing
         /// </summary>
         [JsonProperty(PropertyName = "mediaDuration")]
-        public int? Duration { get; set; }
+        public long? Duration { get; set; }
 
         /// <summary>
         /// Position of the cursor in milliseconds. `null` if not playing
         /// </summary>
         [JsonProperty(PropertyName = "mediaCursor")]
-        public int Cursor { get; set; }
+        public long? Cursor { get; set; }
 
         /// <summary>
         /// Instantiate from JObject
@@ -39,5 +55,17 @@ namespace OBSWebsocketDotNet.Types
         /// Default Constructor
         /// </summary>
         public MediaInputStatus() { }
+    }
+
+    public enum MediaState
+    {
+        OBS_MEDIA_STATE_NONE,
+        OBS_MEDIA_STATE_PLAYING,
+        OBS_MEDIA_STATE_OPENING,
+        OBS_MEDIA_STATE_BUFFERING,
+        OBS_MEDIA_STATE_PAUSED,
+        OBS_MEDIA_STATE_STOPPED,
+        OBS_MEDIA_STATE_ENDED,
+        OBS_MEDIA_STATE_ERROR
     }
 }
