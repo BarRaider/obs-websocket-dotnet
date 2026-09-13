9ad8bec99b2795bc5436fcdab18d45fba9f1a1ac
BarRaider <46548278+BarRaider@users.noreply.github.com>
2026-09-13 16:53:34 +0300

    Fix string conversion in events

--- obs-websocket-dotnet-tests/UnitTest_OBSWebsocket.cs
+++ obs-websocket-dotnet-tests/UnitTest_OBSWebsocket.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 using WebSocketSharp;
 
 namespace OBSWebsocketDotNet.Tests
@@ -14,6 +15,75 @@ namespace OBSWebsocketDotNet.Tests
 
             Assert.IsFalse(msgID.IsNullOrEmpty());
             Assert.AreEqual(idLength, msgID.Length);
+        }
+
+        [TestMethod]
+        public void SceneListChanged_Test()
+        {
+            var body = JObject.Parse(@"{ ""eventData"": { ""scenes"": [ { ""sceneName"": ""Scene 1"" }, { ""sceneName"": ""Scene 2"" } ] } }");
+
+            Types.Events.SceneListChangedEventArgs eventArgs = null;
+            SceneListChanged += (sender, e) => eventArgs = e;
+            ProcessEventType(nameof(SceneListChanged), body);
+
+            Assert.IsNotNull(eventArgs);
+            Assert.AreEqual(2, eventArgs.Scenes.Count);
+            Assert.AreEqual("Scene 1", (string)eventArgs.Scenes[0]["sceneName"]);
+        }
+
+        [TestMethod]
+        public void SceneItemListReindexed_Test()
+        {
+            var body = JObject.Parse(@"{ ""eventData"": { ""sceneName"": ""Scene 1"", ""sceneItems"": [ { ""sceneItemId"": 1 }, { ""sceneItemId"": 2 } ] } }");
+
+            Types.Events.SceneItemListReindexedEventArgs eventArgs = null;
+            SceneItemListReindexed += (sender, e) => eventArgs = e;
+            ProcessEventType(nameof(SceneItemListReindexed), body);
+
+            Assert.IsNotNull(eventArgs);
+            Assert.AreEqual("Scene 1", eventArgs.SceneName);
+            Assert.AreEqual(2, eventArgs.SceneItems.Count);
+            Assert.AreEqual(1, (int)eventArgs.SceneItems[0]["sceneItemId"]);
+        }
+
+        [TestMethod]
+        public void SceneCollectionListChanged_Test()
+        {
+            var body = JObject.Parse(@"{ ""eventData"": { ""sceneCollections"": [ ""Collection 1"", ""Collection 2"" ] } }");
+
+            Types.Events.SceneCollectionListChangedEventArgs eventArgs = null;
+            SceneCollectionListChanged += (sender, e) => eventArgs = e;
+            ProcessEventType(nameof(SceneCollectionListChanged), body);
+
+            Assert.IsNotNull(eventArgs);
+            CollectionAssert.AreEqual(new[] { "Collection 1", "Collection 2" }, eventArgs.SceneCollections);
+        }
+
+        [TestMethod]
+        public void ProfileListChanged_Test()
+        {
+            var body = JObject.Parse(@"{ ""eventData"": { ""profiles"": [ ""Profile 1"", ""Profile 2"" ] } }");
+
+            Types.Events.ProfileListChangedEventArgs eventArgs = null;
+            ProfileListChanged += (sender, e) => eventArgs = e;
+            ProcessEventType(nameof(ProfileListChanged), body);
+
+            Assert.IsNotNull(eventArgs);
+            CollectionAssert.AreEqual(new[] { "Profile 1", "Profile 2" }, eventArgs.Profiles);
+        }
+
+        [TestMethod]
+        public void InputVolumeMeters_Test()
+        {
+            var body = JObject.Parse(@"{ ""eventData"": { ""inputs"": [ { ""inputName"": ""Mic/Aux"" } ] } }");
+
+            Types.Events.InputVolumeMetersEventArgs eventArgs = null;
+            InputVolumeMeters += (sender, e) => eventArgs = e;
+            ProcessEventType(nameof(InputVolumeMeters), body);
+
+            Assert.IsNotNull(eventArgs);
+            Assert.AreEqual(1, eventArgs.inputs.Count);
+            Assert.AreEqual("Mic/Aux", (string)eventArgs.inputs[0]["inputName"]);
         }
 
         [TestMethod]
